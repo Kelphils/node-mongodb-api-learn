53c50c07a276c69af8e323da5fbf3262febebe3c
Kelvin Obioha <obiohakelvin96@gmail.com>
2022-06-07 02:07:14 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const genres = require("./routes/genres");
+const customers = require("./routes/customers");
+const app = express();
+
+mongoose
+  .connect("mongodb://localhost/vidly")
+  .then(() => {
+    console.log("Connected to MongoDB....");
+  })
+  .catch((err) => {
+    console.log("Could not connect to MongoDB......", err);
+  });
+
+app.use(express.json());
+app.use("/api/genres", genres);
+app.use("/api/customers", customers);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
